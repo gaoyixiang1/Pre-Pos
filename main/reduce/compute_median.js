03b286c50777c9fd0601fd664c0231aa6aed4d2f
gaoyixiang1 <45355878+gaoyixiang1@users.noreply.github.com>
2019-08-07 17:36:27 +0800

    Update compute_median.js

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,7 +1,11 @@
 'use strict';
 
+
 function compute_median(collection) {
   //在这里写入代码
+  collection.sort(function(a,b){
+    return a-b;
+  });
   if(collection.length%2!=0){
     return collection[(collection.length-1)/2];
   }else{
